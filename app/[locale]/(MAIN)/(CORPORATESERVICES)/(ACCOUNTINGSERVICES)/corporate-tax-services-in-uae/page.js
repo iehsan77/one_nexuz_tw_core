abc7e12fe51e5fba192f74d82e4ff71df8e86959
Muzammil-nasir <aniq@htconnects.com>
2025-12-17 20:05:13 +0500

    add pages

---
+++ app/[locale]/(MAIN)/(CORPORATESERVICES)/(ACCOUNTINGSERVICES)/corporate-tax-services-in-uae/page.js
@@ -0,0 +1,45 @@
+import { SEOAction } from "@/actions/seo-action";
+import ServicesPage from "@/view/ServicesPages/ServicesPage";
+import React, { Suspense } from "react";
+import ar from "@/locales/ar/corporateTaxServicesInUae.json";
+import en from "@/locales/en/corporateTaxServicesInUae.json";
+import Loader from "@/components/Loader";
+import { isIndex, nocache } from "@/constants/constants";
+
+export async function generateMetadata() {
+  const vMetaData = await SEOAction();
+  return {
+    title: vMetaData?.seo_title || "",
+    description: vMetaData?.seo_description || "",
+    alternates: {
+      canonical: vMetaData?.canonical_url || "",
+    },
+    openGraph: vMetaData?.opengraph_data,
+    twitter: vMetaData?.twitter_tag,
+    robots: {
+      index: isIndex,
+      nocache: nocache,
+    },
+    h1: vMetaData?.h1 || "",
+    faq: vMetaData?.faq?.mainEntity || null,
+    icons: {
+      icon: "/icon.png",
+    },
+  };
+}
+
+export default async function Page({ params }) {
+  const { locale } = await params;
+  const t = locale === "ar";
+  return (
+    <Suspense fallback={<Loader />}>
+      <ServicesPage
+        data={t ? ar : en}
+        locale={t}
+        compare={false}
+        path={locale}
+        sec3={true}
+      />
+    </Suspense>
+  );
+}
